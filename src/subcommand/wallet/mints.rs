b5be4e7997233d9463aa56e6539f0a2339059f72
AAweidai <dw1253464613@gmail.com>
2023-06-27 16:33:59 +0800

    Fix clippy

--- src/subcommand/wallet/mints.rs
+++ src/subcommand/wallet/mints.rs
@@ -325,9 +325,9 @@ impl Mint {
           script_pubkey: destination.script_pubkey(),
           value: 0,
         }];
-        for j in 1..repeat {
+        for item in commit_tx_address.iter().take(repeat).skip(1){
           tx_out.push(TxOut {
-            script_pubkey: commit_tx_address[j].script_pubkey(),
+            script_pubkey: item.script_pubkey(),
             value: 0,
           });
         }
